c1af6ecd1ac4dbeb84a34ea5db8dadf0a2ed059b
satyajit <satyajit_jbl@yahoo.com>
2025-08-02 20:45:38 +0600

    module & env variable setup done

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,21 @@
+/* eslint-disable no-console */
 import {Server} from "http"
 import mongoose from "mongoose";
 import app from "./app";
-import dotenv from 'dotenv';
+import { envVar } from "./app/config/env";
+
 
 let server: Server;
-dotenv.config();
-
 
 
 const startServer = async () => {
 try {
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(envVar.DB_URL as string);
 
     console.log("Connected to DB !!");
     
-    server = app.listen(5000, ()=>{
-        console.log("Server is listening post 5000");
+    server = app.listen(envVar.PORT, ()=>{
+        console.log(`Server is listening post ${envVar.PORT}`);
     })
 } catch (error) {
     console.log(error);
@@ -24,3 +24,66 @@ try {
 
 startServer();
 
+
+
+//Unhandled Rejection error
+
+// process.on("unhandledRejection", (reason, promise)=>{
+//     console.log("Unhandled Rejection detected... Server is shutting down", reason);
+//     console.log("Unhandled Rejection detected... Promise is", promise);
+
+//     if(server){
+//         server.close(()=>{
+//             process.exit(1);
+//         })
+//     };
+//     process.exit(1);
+// })
+process.on("unhandledRejection", (err)=>{
+    console.log("Unhandled Rejection detected... Server is shutting down", err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    };
+    process.exit(1);
+})
+
+//uncaught Exception error
+process.on("uncaughtException", (err)=>{
+    console.log("uncaught Exception detected... Server is shutting down", err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    };
+    process.exit(1);
+})
+
+//Signal termination error(error for manual shutting down by dEployment server ... etc)
+process.on("SIGTERM", (err)=>{
+    console.log("SIGTERM signal received... Server is shutting down", err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    };
+    process.exit(1);
+})
+//testing by manual shutdown
+process.on("SIGINT", (err)=>{
+    console.log("SIGINT signal received... Server is shutting down", err);
+
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    };
+    process.exit(1);
+})
+
+// Promise.reject(new Error("I forgot to cash this promise"))
+// throw new Error("I for to handle this local error")
